4991b169d8899d697d73cf3e5d79819ee4e98308
magnastrazh <magnastrazh@gmail.com>
2017-11-08 09:51:21 +0300

    review commit
    cmd ng -v
    
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/
    
    Angular CLI: 1.5.0
    Node: 8.9.0
    OS: win32 x64
    Angular: 4.2.5
    ... animations, common, compiler, compiler-cli, core, forms
    ... http, platform-browser, platform-browser-dynamic
    ... platform-server, router, tsc-wrapped
    
    @angular/cli: 1.5.0
    @angular-devkit/build-optimizer: 0.0.32
    @angular-devkit/core: 0.0.20
    @angular-devkit/schematics: 0.0.35
    @ngtools/json-schema: 1.1.0
    @ngtools/webpack: 1.5.0
    @schematics/angular: 0.1.1
    typescript: 2.4.1
    webpack-hot-middleware: 2.18.2
    webpack-merge: 4.1.0
    webpack: 2.5.1
    
    Also included test page on /Home/index
    Check please.

--- src/ImageGallery.Client/Controllers/HomeController.cs
+++ src/ImageGallery.Client/Controllers/HomeController.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ImageGallery.Client.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            return View();
+        }
+
         [Authorize]
         public IActionResult About()
         {
             return View();
         }
-
-
     }
 }
--- src/ImageGallery.Client/Startup.cs
+++ src/ImageGallery.Client/Startup.cs
@@ -1,19 +1,13 @@
-﻿using System;
+using System;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using ImageGallery.Client.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using ImageGallery.Client.Services;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.HttpOverrides;
 using Swashbuckle.AspNetCore.Swagger;
@@ -21,17 +15,19 @@ using Microsoft.AspNetCore.DataProtection;
 using StackExchange.Redis;
 using ConfigurationOptions = ImageGallery.Client.Configuration.ConfigurationOptions;
 
+
 namespace ImageGallery.Client
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, IConfiguration configuration)
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -66,11 +62,11 @@ namespace ImageGallery.Client
             });
 
             services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                })
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
                 .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddOpenIdConnect("OpenIdConnect", options =>
                 {
@@ -87,7 +83,7 @@ namespace ImageGallery.Client
                     options.Scope.Add("offline_access");
                     options.Scope.Add("imagegalleryapi");
                     options.Scope.Add("subscriptionlevel");
-                    
+
 
                     options.ResponseType = "code id_token";
                     // CallbackPath = new PathString("...")
@@ -114,7 +110,8 @@ namespace ImageGallery.Client
             services.AddScoped<IImageGalleryHttpClient, ImageGalleryHttpClient>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -125,6 +122,7 @@ namespace ImageGallery.Client
             {
                 app.UseExceptionHandler("/Shared/Error");
             }
+
 
             var forwardOptions = new ForwardedHeadersOptions
             {
@@ -137,69 +135,11 @@ namespace ImageGallery.Client
 
             app.UseForwardedHeaders(forwardOptions);
 
-            #region Moved to Configure Services 
-
-            //app.UseCookieAuthentication(new CookieAuthenticationOptions
-            //{
-            //    AuthenticationScheme = "Cookies",
-            //    AccessDeniedPath = "/Authorization/AccessDenied"
-            //});
-
-            #endregion 
-
 
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
             var config = Configuration.Get<ConfigurationOptions>();
             Console.WriteLine("Authority" + config.OpenIdConnectConfiguration.Authority);
-
-            #region Moved
-            //app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
-            //{
-            //    RequireHttpsMetadata = false,
-
-            //    AuthenticationScheme = "oidc",
-            //    Authority = config.OpenIdConnectConfiguration.Authority, 
-            //    ClientId = config.OpenIdConnectConfiguration.ClientId,
-            //    Scope = { "openid", "profile", "address", "roles", "imagegalleryapi", "subscriptionlevel", "country", "offline_access" },
-            //    ResponseType = "code id_token",
-            //    // CallbackPath = new PathString("...")
-            //    //SignedOutCallbackPath = new PathString("")
-            //    SignInScheme = "Cookies",
-            //    SaveTokens = true,
-            //    ClientSecret = config.OpenIdConnectConfiguration.ClientSecret,
-            //    GetClaimsFromUserInfoEndpoint = true,
-            //    Events = new OpenIdConnectEvents()
-            //    {
-            //        OnTokenValidated = tokenValidatedContext =>
-            //        {
-            //            var identity = tokenValidatedContext.Ticket.Principal.Identity
-            //                as ClaimsIdentity;
-
-            //            var subjectClaim = identity.Claims.FirstOrDefault(z => z.Type == "sub");
-
-            //            var newClaimsIdentity = new ClaimsIdentity(
-            //                tokenValidatedContext.Ticket.AuthenticationScheme,
-            //                "given_name",
-            //                "role");
-
-            //            newClaimsIdentity.AddClaim(subjectClaim);
-
-            //            tokenValidatedContext.Ticket = new AuthenticationTicket(
-            //                new ClaimsPrincipal(newClaimsIdentity),
-            //                tokenValidatedContext.Ticket.Properties,
-            //                tokenValidatedContext.Ticket.AuthenticationScheme);
-
-            //            return Task.FromResult(0);
-            //        },
-            //        OnUserInformationReceived = userInformationReceivedContext =>
-            //        {
-            //            userInformationReceivedContext.User.Remove("address");
-            //            return Task.FromResult(0);
-            //        }
-            //    }
-            //});
-            #endregion
 
             app.UseAuthentication();
 
@@ -217,6 +157,10 @@ namespace ImageGallery.Client
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Gallery}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
